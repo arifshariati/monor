306c73eba75b4426b5e5b012c8477ae76fb5c457
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-26 16:17:29 +0200

    refactor(ai-content-generator): removed sample files

--- apps/ai-content-generator/src/app/sample.spec.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import Sample from './sample';
-describe('Sample', () => {
-  it('should load component', () => {
-    const { baseElement } = render(<Sample />);
-    expect(baseElement).toBeTruthy();
-  });
-});
