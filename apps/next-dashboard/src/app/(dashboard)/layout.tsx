13306dc4b38e4c32e9adfbe1493a2acb903d8485
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-02 10:28:41 +0200

    refactor(next-dashboard): updated forms layout

--- apps/next-dashboard/src/app/(dashboard)/layout.tsx
+++ apps/next-dashboard/src/app/(dashboard)/layout.tsx
@@ -9,9 +9,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <Header />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-          {children}
-        </main>
+        <main className="flex min-h-screen p-10">{children}</main>
       </div>
     </div>
   );
