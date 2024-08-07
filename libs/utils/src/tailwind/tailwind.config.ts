81ce51a35bb879adcd3aad20aafdf821d405b429
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-07 17:46:39 +0200

    feat(core): added font family key to global tailwind css config

--- libs/utils/src/tailwind/tailwind.config.ts
+++ libs/utils/src/tailwind/tailwind.config.ts
@@ -69,6 +69,10 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      fontFamily: {
+        inter: 'var(--font-inter)',
+        lexend: 'var(--font-lexend)',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
