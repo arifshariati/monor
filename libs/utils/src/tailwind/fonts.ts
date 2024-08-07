bb38e8875e415ffebe7ff5421cee6e90deaf7aad
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-07 17:46:13 +0200

    feat(core): made next fonts reusable

---
+++ libs/utils/src/tailwind/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lexend } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+export const lexend = Lexend({ subsets: ['latin'], variable: '--font-lexend' });
