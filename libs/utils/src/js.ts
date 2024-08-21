f1c93436f4b9ece349d7b7ea7aeec6578c199c55
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-21 17:34:17 +0200

    feat(core): add string first letter

--- libs/utils/src/js.ts
+++ libs/utils/src/js.ts
@@ -4,3 +4,8 @@ export const stringify = (value: any, spacing = 2) =>
   JSON.stringify(value, null, 2);
 
 export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const stringFirstLetterCapital = (str: string) => {
+  if (!str.length) return '';
+  return str.charAt(0).toUpperCase();
+};
