8b71d09099c701e1add81be8477a110e4059b6af
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-21 13:44:53 +0200

    refactor(banking): refactor signup and signin forms to submit form values

--- apps/banking/src/components/forms/sign-up/sign-up-form.tsx
+++ apps/banking/src/components/forms/sign-up/sign-up-form.tsx
@@ -1,12 +1,27 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { DynamicForm } from '@monor/ui/dynamic-form';
+import { toast } from '@monor/ui/shadcn/use-toast';
 import { formConfig } from './sign-up-form-config';
+import { signUp } from '../../../actions/auth.actions';
 
 const SignUpForm = () => {
+  const router = useRouter();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handleSubmit = (values: any) => {
+  const handleSubmit = async (values: any) => {
     console.log(values);
+    try {
+      const userAccountCreated = await signUp(values);
+      router.push('/');
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast({
+          title: '🔴 Error',
+          description: error.message,
+        });
+      }
+    }
   };
   return (
     <div className="p-4 w-full lg:w-[500px]">
