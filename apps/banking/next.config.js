3172da8db74a89024f13013bfcc20177468ff3e4
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-08-22 16:19:54 +0200

    feat(banking): add initial sentry setup

--- apps/banking/next.config.js
+++ apps/banking/next.config.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const { withSentryConfig } = require("@sentry/nextjs")
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -12,7 +13,28 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  sentry: {
+    hideSourceMaps: true,
+  }
 };
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
+  org: process.env['NEXT_PUBLIC_SENTRY_ORG'],
+  project: process.env['NEXT_PUBLIC_SENTRY_PROJECT'],
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env['NEXT_PUBLIC_AUTH_TOKEN'],
+  silent: true, // Suppresses all logs
+
+
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
@@ -20,3 +42,8 @@ const plugins = [
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = async (/** @type {string} */phase) => {
+  const nxConfig = withNx(nextConfig)
+  const config = await nxConfig(phase)
+  return withSentryConfig(config, sentryWebpackPluginOptions)
+}
